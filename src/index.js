5e66314a966c5361ff0d719c647fc19387b2902e
shadab khan <shadabkhan2910@gmail.com>
2025-10-25 16:18:03 +0530

    testing lint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 // create a server
+// var x = 10
 
+// someUndefinedVariable = 'This should cause an error';
 const server = http.createServer((req, res) => {
   res.end('pro-node-js-setup after adding watch');
 });
